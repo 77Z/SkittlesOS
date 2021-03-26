8354c6b0583085d0e4abe8629a5702d88f60f1f1
Vince R <r2d2fan20@outlook.com>
2021-03-25 23:06:41 -0500

    VBox Batch

---
+++ WINVboxBootFromUSB.bat
@@ -0,0 +1,3 @@
+REM HEY DON'T EXECUTE THIS UNTIL YOU CHANGE THE DRIVE NUMBER
+
+%programfiles%\Oracle\VirtualBox\VBoxManage.exe internalcommands createrawvmdk -filename C:\usb.vmdk -rawdisk \\.\PhysicalDrive1
